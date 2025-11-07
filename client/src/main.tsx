2766438798ccab0982706d57a0e2d3fc333f4d66
Aduragbemi_Soladoye <segunolaotan90@gmail.com>
2025-11-07 21:30:54 +0100

    Change client directory

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,17 +1,23 @@
 import { initThemeMode } from "flowbite-react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router";
 import { ThemeInit } from "../.flowbite-react/init";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router";
+
 import App from "./App.tsx";
 import "./index.css";
+import Products from "./components/Products.tsx";
+import Product from "./pages/Product.tsx";
+
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeInit />
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/:product" element={<Product />} />
+      </Routes>
+    </Router>
   </StrictMode>,
 );
 
