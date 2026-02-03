658161332641cb9f2f91c609e3d5ca8515d3dffe
Aduragbemi_Soladoye <segunolaotan90@gmail.com>
2026-02-03 02:24:30 +0100

    Create slice

---
+++ redux/store/slices/product.ts
@@ -0,0 +1,42 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import fetchProductThunk from '../thunks/product.ts'
+import { Product } from "../../../client/src/interface.ts";
+
+
+interface ProductStateType {
+  product: Product | null;
+  loading: boolean;
+  error: string | null;
+}
+
+const initialState: ProductStateType = {
+  product: null,
+  loading: false,
+  error: null,
+};
+
+
+const ProductSlice = createSlice({
+  name: "product",
+  initialState,
+  reducers: {},
+  extraReducers: (builder)=>{
+    builder
+        .addCase(fetchProductThunk.pending, (state)=>{
+          state.loading = true
+        })
+        .addCase(fetchProductThunk.fulfilled, (state, action)=>{
+          state.loading = false
+          state.product = action.payload
+          state.error = null
+        })
+        .addCase(fetchProductThunk.rejected, (state, action)=>{
+          state.loading = false
+          state.error = action.error.message! || action.payload!
+          state.product = null
+        })
+  }
+});
+
+
+export default ProductSlice.reducer;
