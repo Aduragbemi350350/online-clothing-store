fd7878c51037cb017c6bcfc9769e11535fc86a7b
Aduragbemi_Soladoye <segunolaotan90@gmail.com>
2026-02-03 02:23:12 +0100

    Add comments and user slice

--- redux/store/store.tsx
+++ redux/store/store.tsx
@@ -1,16 +1,22 @@
 //createStore object
 
 import {configureStore} from '@reduxjs/toolkit'
-import productsSlice from './slice/products'
-import productSlide from './slice/product'
+import productsSlice from './slices/products'
+import productSlice from './slices/product'
+import commentSlice from './slices/comment'
+import userSlice from './slices/user'
 
 const store = configureStore({
     reducer: {
         products: productsSlice,
-        product: productSlide
+        product: productSlice,
+        comments: commentSlice, 
+        user: userSlice
     }
 })
 
-export type RootState = ReturnType<typeof store.getState>;
+export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
 export default store;
+
+
