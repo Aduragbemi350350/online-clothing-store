cdd18ca35ca7c2f0fdffdf54852499791595d66f
Aduragbemi_Soladoye <segunolaotan90@gmail.com>
2026-02-03 02:12:52 +0100

    Add pages- Signup, Signin and AddProduct.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -9,6 +9,9 @@ import "./index.css";
 import Product from "./pages/Product.tsx";
 import store from "../../redux/store/store.tsx";
 import { Provider } from "react-redux";
+import Signup from "./pages/Signup.tsx";
+import Signin from "./pages/Signin.tsx";
+import AddProduct from "./pages/addProduct.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -17,6 +20,9 @@ createRoot(document.getElementById("root")!).render(
         <Routes>
           <Route path="/" element={<App />} />
           <Route path="/:slug" element={<Product />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/product/create" element={< AddProduct/>} />
         </Routes>
       </Router>
     </Provider>
