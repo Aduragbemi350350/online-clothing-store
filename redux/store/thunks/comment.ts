89a1bcfc6d79817b47aead3d3639fbb59edc88fe
Aduragbemi_Soladoye <segunolaotan90@gmail.com>
2026-02-03 02:24:51 +0100

    Create thunks

---
+++ redux/store/thunks/comment.ts
@@ -0,0 +1,17 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { Comment } from "../../../client/src/interface";
+import axios from "axios";
+
+
+export const fetchCommentsThunk = createAsyncThunk<Comment[], void, {rejectValue: string}>('comments/fetchComments', async(_,thunkAPI)=>{
+    try {
+        const response = await axios.get('http://localhost:3000/api/comments/', {withCredentials:true})
+        
+        console.log({fetchComment: response})
+        if(!response) return thunkAPI.rejectWithValue("No response!")
+
+            return response.data
+    } catch (error) {
+        thunkAPI.rejectWithValue("Request unsuccessful!")
+    }
+})
