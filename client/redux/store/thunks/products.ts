2495f4fe09812f49a305d9f0cc66d2e97b0ca10d
Aduragbemi_Soladoye <segunsoladoye80@gmail.com>
2026-02-04 09:41:08 +0100

    Change get url

--- client/redux/store/thunks/products.ts
+++ client/redux/store/thunks/products.ts
@@ -2,16 +2,22 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import { Product } from "../../../src/interface";
 import axios from "axios";
 
+export const fetchProductsThunk = createAsyncThunk<
+  Product[],
+  void,
+  { rejectValue: string }
+>("products/fetchProducts", async (_, thunkAPI) => {
+  try {
+    const response = await axios.get(
+      "https://online-clothing-store-zay8.onrender.com/api/products",
+      { withCredentials: true },
+    );
 
-export const fetchProductsThunk = createAsyncThunk<Product[], void,  { rejectValue: string }>('products/fetchProducts', async(_, thunkAPI)=>{
-    try {
-        const response = await axios.get('http://localhost:3000/api/products', {withCredentials: true})
+    if (!response) throw new Error("No response!");
+    return response.data as Product[];
+  } catch (error) {
+    return thunkAPI.rejectWithValue("Request failed!");
+  }
+});
 
-        if(!response) throw new Error("No response!")
-            return response.data as Product[]
-    } catch (error) {
-        return thunkAPI.rejectWithValue("Request failed!")
-    }
-})
-
-export default fetchProductsThunk
+export default fetchProductsThunk;
