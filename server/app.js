7db54a00b4b898fb41e1187481754266c30dad26
Aduragbemi_Soladoye <segunsoladoye80@gmail.com>
2026-02-04 10:06:36 +0100

    Remove tailing slash

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors({
     origin: [
         "http://localhost:5173/",
-        "https://online-clothing-store-iota.vercel.app/"],
+        "https://online-clothing-store-iota.vercel.app"],
     credentials: true
 }));
 app.use(express.json())
