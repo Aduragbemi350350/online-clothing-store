d24f97b1ff3ed2ff367eee9a51f80549686a2b87
Aduragbemi_Soladoye <segunsoladoye80@gmail.com>
2026-02-07 16:49:21 +0100

    Refactor code, change the error handling conditions

--- client/redux/store/thunks/comment.ts
+++ client/redux/store/thunks/comment.ts
@@ -2,17 +2,44 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import { Comment } from "../../../src/interface";
 import axios from "axios";
 
+export const fetchCommentsThunk = createAsyncThunk<
+  Comment[],
+  void,
+  { rejectValue: any }
+>("comments/fetchComments", async (_, thunkAPI) => {
+  try {
+    const response = await axios.get("http://localhost:3000/api/comments/", {
+      withCredentials: true,
+    });
 
-
-export const fetchCommentsThunk = createAsyncThunk<Comment[], void, {rejectValue: string}>('comments/fetchComments', async(_,thunkAPI)=>{
-    try {
-        const response = await axios.get('http://localhost:3000/api/comments/', {withCredentials:true})
-        
-        console.log({fetchComment: response})
-        if(!response) return thunkAPI.rejectWithValue("No response!")
-
-            return response.data
-    } catch (error) {
-        thunkAPI.rejectWithValue("Request unsuccessful!")
+    console.log({
+      mess: "Fetch comments from DB",
+      comments: response,
+    });
+    if (!response)
+      return thunkAPI.rejectWithValue({
+        message: "No response",
+        status: 404,
+      });
+    return response.data;
+  } catch (error: any) {
+    let errMessage;
+    if (error.response.status === 500 || error.status === 500) {
+      errMessage = {
+        name: "Unknown error",
+        message: "Internal server error",
+        status: 500,
+      };
+      return thunkAPI.rejectWithValue(errMessage);
     }
-})
+
+    errMessage = error?.response.data;
+
+    console.log({
+      mess: "Fetch comments from DB failed",
+      comments: errMessage,
+    });
+
+    return thunkAPI.rejectWithValue(errMessage);
+  }
+});
