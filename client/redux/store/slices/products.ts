40376b3bbe16af932f9cc0f57417f89289981a39
Aduragbemi_Soladoye <segunsoladoye80@gmail.com>
2026-02-07 16:48:16 +0100

    Remove tailing !

--- client/redux/store/slices/products.ts
+++ client/redux/store/slices/products.ts
@@ -35,7 +35,7 @@ const productsSlice = createSlice({
       .addCase(fetchProductsThunk.rejected, (state, action)=>{
         state.loading = false,
         state.products = [],
-        state.error = action.payload!
+        state.error = action.payload
       })
   }
 })
