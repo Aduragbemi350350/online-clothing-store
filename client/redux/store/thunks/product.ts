5c17c6b0b803187d86bdcb056edf89de81c3b4e0
Aduragbemi_Soladoye <segunsoladoye80@gmail.com>
2026-02-07 16:50:46 +0100

    Change reject value type and refactor error handling conditions

--- client/redux/store/thunks/product.ts
+++ client/redux/store/thunks/product.ts
@@ -5,20 +5,47 @@ import { Product } from "../../../src/interface";
 export const fetchProductThunk = createAsyncThunk<
   Product,
   string,
-  { rejectValue: string }
+  { rejectValue: any }
 >("product/fetchProduct", async (slug, thunkAPI) => {
   try {
     const response = await axios.get(
       `http://localhost:3000/api/products/${slug}`,
-      {withCredentials: true}
+      { withCredentials: true },
     );
 
-    console.log({productResponse: response})
-    if (!response) throw new Error("No product!");
+    console.log({ productResponse: response });
+    if (!response)
+      return thunkAPI.rejectWithValue({
+        name: "Document not found",
+        message: "No response from the DB",
+        status: 400,
+      });
+
     return response.data as Product;
-  } catch (error) {
-    console.log({responseError: error})
-    return thunkAPI.rejectWithValue("Request failed")
+  } catch (error: any) {
+    let errMessage;
+    if (error.response.status === 500 || error.status === 500) {
+      errMessage = {
+        name: "Unknown error",
+        message: "Internal server error",
+        status: 500,
+      };
+
+      console.log({
+        mess: "Fetch product from DB failed",
+        product: errMessage,
+      });
+      return thunkAPI.rejectWithValue(errMessage);
+    }
+
+    errMessage = error?.response.data;
+
+    console.log({
+      mess: "Fetch product from DB failed",
+      product: errMessage,
+    });
+
+    return thunkAPI.rejectWithValue(errMessage);
   }
 });
 
