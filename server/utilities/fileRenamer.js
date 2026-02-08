6971233511475a78756be313d9a925717f16d1b5
Aduragbemi_Soladoye <segunsoladoye80@gmail.com>
2026-02-08 12:54:30 +0100

    Refactor code

--- server/utilities/fileRenamer.js
+++ server/utilities/fileRenamer.js
@@ -3,7 +3,7 @@ import path from "path"
 export default function fileRenamer(originalName){
     const extname = path.extname(originalName)
     const basename = path.basename(originalName, extname)
-    const filteredBasename = basename.replace(/\s+/g, "-").replace(/[^a-zA-Z0-9-_]/,"")
+    const filteredBasename = basename.replace(/\s+/g, "").replace(/[^a-zA-Z0-9-_]/,"-")
 
     const name = `${filteredBasename}-${Date.now()}${extname}`
     return name
