d50a99810b68deabad4e5770b8e1c9697fbab4ba
Aduragbemi_Soladoye <segunsoladoye80@gmail.com>
2026-02-10 12:03:16 +0100

    Make email field unique and add the new field- createdOtpAt.

--- server/models/User.js
+++ server/models/User.js
@@ -3,12 +3,13 @@ import mongoose from 'mongoose'
 
 const userSchema = mongoose.Schema({
     username: {type: String, required: true},
-    email: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
     role: {type: String, enum: ["user", "admin"]},
     password: {type: String, require: true},
     otp: String,
     otpExpiresAt: Number,
-    verified: {type: Boolean, default: false}
+    verified: {type: Boolean, default: false},
+    createdOtpAt: {type: Date}
 })
 
 const User = mongoose.model("User", userSchema)
