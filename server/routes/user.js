f80b146977fb56e4bb7c24cc901269a18f4b0930
Aduragbemi_Soladoye <segunsoladoye80@gmail.com>
2026-02-10 12:03:55 +0100

    Add verifyotp and requestotp. Refactor route positions and structure.

--- server/routes/user.js
+++ server/routes/user.js
@@ -4,22 +4,26 @@ dns.setDefaultResultOrder('ipv4first')
 
 
 const router = express.Router()
-import { createUser, currentUser, deleteUser, deleteUsers, getUsers, login, logout } from '../controllers/user.js'
+import { createUser, currentUser, deleteUser, deleteUsers, getUsers, login, logout, requestOTP, verifyOTP } from '../controllers/user.js'
 import { getUser } from '../auth/authMiddleware.js'
 
 router
     .get('/', getUsers)
 
-    .delete('/:id', deleteUser)
-
-    .get('/currentUser', getUser, currentUser)
-
-    .post('/', createUser)
-
-    .post('/login', login)
+    .get('/currentuser', getUser, currentUser)
 
     .get('/logout', logout)
 
+    .post('/', createUser)
+
+    .post('/verifyotp', verifyOTP)
+
+    .post('/requestotp', requestOTP)
+
+    .post('/login', login)
+
     .delete('/', deleteUsers)
+
+    .delete('/:id', deleteUser)
 
 export default router
