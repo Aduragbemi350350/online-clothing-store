b641ff615f1336a63a2f369de46983e6687c69ce
Aduragbemi_Soladoye <segunsoladoye80@gmail.com>
2026-02-18 21:13:17 +0100

    Create image schema

--- server/models/Product.js
+++ server/models/Product.js
@@ -1,11 +1,23 @@
 import mongoose from "mongoose";
 import  Review  from "./Review.js";
 
+
+//image schema
+const imageSchema = mongoose.Schema({
+    publicKey: String,
+    secureURL: String,
+    resourceType: String,
+    folder: String,
+    originalFilename: String
+})
+
+
+//product schema
 const productSchema = mongoose.Schema({
     name: {type: String, required: true},
     description: {type: String, required: true},
     category: {type: String, enum: ["ankara", "tshirt", "jeans", "hoodie", "jacket"] ,required: true},
-    image: {type: String, required: true},
+    images: [imageSchema],
     price: {type: Number, required: true},
     ratings: [
         {
