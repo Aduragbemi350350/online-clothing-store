83b0082579a01fe3dec9efeb7b07208f552ac0ec
Aduragbemi_Soladoye <segunsoladoye80@gmail.com>
2026-02-18 21:07:42 +0100

    Add image interface

--- client/src/interface.ts
+++ client/src/interface.ts
@@ -1,19 +1,21 @@
 export interface Comment {
-  _id: string
+  _id: string;
   user: string;
   product: string;
   text: string;
   like: string[];
   dislike: string[];
-  parent: string ;
+  parent: string;
   createdAt: string;
   updatedAt: string;
 }
 
-export interface ProductReview {
-  rating: number;
-  like: string;
-  comments: string[];
+export interface Image {
+  publicKey: string;
+  secureURL: string;
+  resourceType: string;
+  folder: string;
+  originalFilename: string;
 }
 
 export interface Product {
@@ -21,16 +23,15 @@ export interface Product {
   name: string;
   description: string;
   category: string;
-  image: string;
+  image: Image[];
   price: number;
   createdBy: string;
   slug: string;
   rating: number;
-  reviews: ProductReview[];
 }
 
-export interface User{
-  username: string,
-  password: string,
-  email: string
+export interface User {
+  username: string;
+  password: string;
+  email: string;
 }
