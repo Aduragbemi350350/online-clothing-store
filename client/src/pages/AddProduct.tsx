d89d879874f58573fb2aff452fc85f987d83e79a
Aduragbemi_Soladoye <segunsoladoye80@gmail.com>
2026-02-18 22:45:21 +0100

    Refactor code

--- client/src/pages/AddProduct.tsx
+++ client/src/pages/AddProduct.tsx
@@ -134,7 +134,9 @@ export default function AddProduct() {
                   name="category"
                   className="focus:ring-primary-500 focus:border-primary-500 dark:focus:ring-primary-500 dark:focus:border-primary-500 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400"
                 >
-                  <option defaultValue={""}>Select category</option>
+                  <option disabled={true} selected={true}>
+                    Select category
+                  </option>
                   <option value="ankara">Ankara</option>
                   <option value="tshirt">Tshirt</option>
                   <option value="jeans">Jeans</option>
