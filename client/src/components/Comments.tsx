5c4f723ebe9ecb64f9f459e7f5071f6b3e5463e9
Aduragbemi_Soladoye <segunsoladoye80@gmail.com>
2026-02-19 22:21:51 +0100

    Modify UI to inform user if there's no comment

--- client/src/components/Comments.tsx
+++ client/src/components/Comments.tsx
@@ -1,12 +1,11 @@
 import CommentArticle from "./CommentArticle";
-import {  Product } from "../interface";
-import {  useSelector } from "react-redux";
-import {  RootState } from "../../redux/store/store";
-
+import { Product } from "../interface";
+import { useSelector } from "react-redux";
+import { RootState } from "../../redux/store/store";
 
 const Comments = ({ product }: { product: Product }) => {
   // const dispatch = useDispatch<AppDispatch>();
-  
+
   const { loading, comments, error } = useSelector(
     (state: RootState) => state.comments,
   );
@@ -16,27 +15,29 @@ const Comments = ({ product }: { product: Product }) => {
   //RETURNS
 
   //loading
-  if(loading) return <p>Comment loading...</p>
+  if (loading) return <p>Comment loading...</p>;
 
   //error
   console.log({
-    commentError: error
-  })
-  if(error) return <p className="h3 mx-30">{`No comments: ${error.message}`}</p>
+    commentError: error,
+  });
+  if (error)
+    return <p className="h3 mx-30">{`No comments: ${error.message}`}</p>;
 
   //comments
   return (
     <section>
-      {comments.map(
-        (comment) =>
-          (
-            <CommentArticle
-              key={comment._id}
-              comment={comment}
-              product={product._id}
-            />
-          ),
-      )}
+      { comments.length > 0 ? comments.map((comment) => (
+        <CommentArticle
+          key={comment._id}
+          comment={comment}
+          product={product._id}
+        />
+      )) : 
+      <div>
+        <h2>Product doesn't have comments</h2>
+      </div>
+      }
     </section>
   );
 };
