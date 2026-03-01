2b44b38884f94b834a1059c51ac21ae080d08fca
Aduragbemi_Soladoye <segunsoladoye80@gmail.com>
2026-03-01 20:48:10 +0100

    Refactor code. Build reducer to reset product state.

--- client/redux/store/slices/product.ts
+++ client/redux/store/slices/product.ts
@@ -1,7 +1,7 @@
-import { createSlice} from "@reduxjs/toolkit";
-import fetchProductThunk from '../thunks/product.ts'
+import { createSlice } from "@reduxjs/toolkit";
+import fetchProductThunk from "../thunks/product.ts";
 import { Product } from "../../../src/interface";
-
+import reducer from "./products.ts";
 
 interface ProductStateType {
   product: Product | null;
@@ -15,28 +15,36 @@ const initialState: ProductStateType = {
   error: null,
 };
 
-
 const ProductSlice = createSlice({
   name: "product",
   initialState,
-  reducers: {},
-  extraReducers: (builder)=>{
+  reducers: {
+    resetProduct(state) {
+      state.loading = false;
+      state.product = null;
+      state.error = null;
+    },
+  },
+  extraReducers: (builder) => {
     builder
-        .addCase(fetchProductThunk.pending, (state)=>{
-          state.loading = true
-        })
-        .addCase(fetchProductThunk.fulfilled, (state, action)=>{
-          state.loading = false
-          state.product = action.payload
-          state.error = null
-        })
-        .addCase(fetchProductThunk.rejected, (state, action)=>{
-          state.loading = false
-          state.error = action.payload
-          state.product = null
-        })
-  }
+      .addCase(fetchProductThunk.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(fetchProductThunk.fulfilled, (state, action) => {
+        state.loading = false;
+        state.product = action.payload;
+        state.error = null;
+      })
+      .addCase(fetchProductThunk.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+        state.product = null;
+      });
+  },
 });
 
+export const {
+  resetProduct
+} = ProductSlice.actions
 
 export default ProductSlice.reducer;
