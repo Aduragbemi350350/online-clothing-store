3089942de1311b3cd7a3b7d3f02c8e2a6a56f9f2
Aduragbemi_Soladoye <segunsoladoye80@gmail.com>
2026-03-01 20:46:44 +0100

    Refactor code. Add reducer to reset comment state.

--- client/redux/store/slices/comment.ts
+++ client/redux/store/slices/comment.ts
@@ -2,41 +2,46 @@ import { createSlice } from "@reduxjs/toolkit";
 import { Comment } from "../../../src/interface";
 import { fetchCommentsThunk } from "../thunks/comment";
 
-
-interface CommentState{
-    loading: boolean,
-    comments: Comment[],
-    error : any
+interface CommentState {
+  loading: boolean;
+  comments: Comment[];
+  error: any;
 }
 
-const initialState : CommentState = {
-    loading: false,
-    comments: [],
-    error:  null
+const initialState: CommentState = {
+  loading: false,
+  comments: [],
+  error: null,
 };
 
 const commentSlice = createSlice({
   name: "comment",
   initialState,
-  reducers: {},
-  extraReducers: (builder)=>{
+  reducers: {
+    resetComment(state) {
+      ((state.loading = false), (state.comments = []), (state.error = null));
+    },
+  },
+  extraReducers: (builder) => {
     builder
-        .addCase( fetchCommentsThunk.pending , (state)=>{
-            state.loading = true,
-            state.comments = [],
-            state.error = null
-        })
-        .addCase( fetchCommentsThunk.fulfilled, (state, action)=>{
-            state.comments = action.payload,
-            state.loading = false,
-            state.error = null
-        })
-        .addCase( fetchCommentsThunk.rejected, (state, action)=>{
-            state.comments = [],
-            state.loading = false,
-            state.error = action.payload!
-        })
-  }
+      .addCase(fetchCommentsThunk.pending, (state) => {
+        ((state.loading = true), (state.comments = []), (state.error = null));
+      })
+      .addCase(fetchCommentsThunk.fulfilled, (state, action) => {
+        ((state.comments = action.payload),
+          (state.loading = false),
+          (state.error = null));
+      })
+      .addCase(fetchCommentsThunk.rejected, (state, action) => {
+        ((state.comments = []),
+          (state.loading = false),
+          (state.error = action.payload!));
+      });
+  },
 });
+
+export const {
+    resetComment
+} = commentSlice.actions
 
 export default commentSlice.reducer;
