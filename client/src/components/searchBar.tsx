6cd6a168b0828d4ab26f289601784725862b1d16
Aduragbemi_Soladoye <segunsoladoye80@gmail.com>
2026-03-03 11:17:07 +0100

    Create the logic to remove dropdown when the window is clicked.

--- client/src/components/searchBar.tsx
+++ client/src/components/searchBar.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useDispatch } from "react-redux";
 import fetchProductsThunk from "../../redux/store/thunks/products";
 import { AppDispatch } from "../../redux/store/store";
@@ -43,12 +43,39 @@ const SearchBar = () => {
     });
   }
 
+  // get elements
+  const categoryRef = useRef<SVGSVGElement>(null);
+  const priceRef = useRef<SVGSVGElement>(null);
+
+  useEffect(() => {
+    const handleWindowClick = (e: Event) => {
+      //close all opened tab
+      if (
+        categoryRef.current &&
+        categoryRef.current !== e.target &&
+        openCategory
+      ) {
+        setOpenCategory(false);
+      } else if (
+        priceRef.current &&
+        priceRef.current !== e.target &&
+        openPrice
+      ) {
+        setOpenPrice(false);
+      }
+    };
+
+    window.addEventListener("click", handleWindowClick);
+
+    return () => window.removeEventListener("click", handleWindowClick);
+  }, [openCategory, openPrice]);
+
   return (
     <section>
       <form className="flex pt-26">
-        <div className="mx-16 flex w-full justify-center">
+        <div className="mx-16 flex w-full flex-col justify-center gap-y-1 md:flex-row">
           {/* category */}
-          <div className="jusify-center relative flex items-center gap-x-2 rounded-l-lg border border-gray-400 px-6 py-3">
+          <div className="jusify-center relative flex items-center gap-x-2 border border-gray-400 px-6 py-3 md:rounded-l-lg">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -63,23 +90,28 @@ const SearchBar = () => {
                 d="M3.75 6A2.25 2.25 0 0 1 6 3.75h2.25A2.25 2.25 0 0 1 10.5 6v2.25a2.25 2.25 0 0 1-2.25 2.25H6a2.25 2.25 0 0 1-2.25-2.25V6ZM3.75 15.75A2.25 2.25 0 0 1 6 13.5h2.25a2.25 2.25 0 0 1 2.25 2.25V18a2.25 2.25 0 0 1-2.25 2.25H6A2.25 2.25 0 0 1 3.75 18v-2.25ZM13.5 6a2.25 2.25 0 0 1 2.25-2.25H18A2.25 2.25 0 0 1 20.25 6v2.25A2.25 2.25 0 0 1 18 10.5h-2.25a2.25 2.25 0 0 1-2.25-2.25V6ZM13.5 15.75a2.25 2.25 0 0 1 2.25-2.25H18a2.25 2.25 0 0 1 2.25 2.25V18A2.25 2.25 0 0 1 18 20.25h-2.25A2.25 2.25 0 0 1 13.5 18v-2.25Z"
               />
             </svg>
-            <button type="button">Category</button>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="size-4"
-              onClick={() => setOpenCategory((state) => !state)}
+            <button
+              type="button"
+              className="flex items-center justify-center gap-2"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="m19.5 8.25-7.5 7.5-7.5-7.5"
-              />
-            </svg>
-
+              Category
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="size-4"
+                ref={categoryRef}
+                onClick={() => setOpenCategory((state) => !state)}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m19.5 8.25-7.5 7.5-7.5-7.5"
+                />
+              </svg>
+            </button>
             {/* dropdown */}
             <ul
               className={
@@ -113,23 +145,28 @@ const SearchBar = () => {
               />
             </svg>
 
-            <button type="button">Price</button>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="size-4"
-              onClick={() => setOpenPrice((state) => !state)}
+            <button
+              type="button"
+              className="flex items-center justify-center gap-2"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="m19.5 8.25-7.5 7.5-7.5-7.5"
-              />
-            </svg>
-
+              Price
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="size-4"
+                ref={priceRef}
+                onClick={() => setOpenPrice((state) => !state)}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m19.5 8.25-7.5 7.5-7.5-7.5"
+                />
+              </svg>
+            </button>
             {/* dropdown */}
             <div
               className={
@@ -186,13 +223,13 @@ const SearchBar = () => {
           </div>
 
           <input
-            className="border border-gray-400 ps-4 pe-8 focus:outline-none"
+            className="border border-gray-400 py-3 ps-4 pe-8 focus:outline-none"
             type="search"
-            placeholder="name, description ..."
+            placeholder="Product name, description ..."
             onChange={(e) => setQuery(e.currentTarget.value)}
           />
 
-          <div className="jusify-center flex items-center gap-x-2 rounded-e-lg border border-gray-400 px-6 py-3">
+          <div className="jusify-center flex items-center gap-x-2 border border-gray-400 px-6 py-3 md:rounded-e-lg">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
